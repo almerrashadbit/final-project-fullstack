aff907055f2af2f0f7706c62e0d59f928833e195
almerrashadbit <almer.rashad@bithealth.co.id>
2024-03-25 15:30:11 +0700

    Second Commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign in</h1>
+          <p class="text-xs-center">
+            <!-- <router-link :to="{ name: 'register' }">
+              Need an account?
+            </router-link> -->
+          </p>
+          <ul v-if="errors" class="error-messages">
+            <li v-for="(v, k) in errors" :key="k">{{ k }} {{ v | error }}</li>
+          </ul>
+          <form @submit.prevent="onSubmit(email, password)">
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                v-model="email"
+                placeholder="Email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                v-model="password"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button class="btn btn-sm btn-primary pull-xs-right">
+              Sign in
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const email = ref('')
+const password = ref('')
+
+
+</script>
